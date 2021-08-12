fb5b2e9f0e434f6c1dbd1e156f6c82335fdf1ac1
ayeong <ahng1223@gmail.com>
2021-08-12 15:41:31 +0900

    등대박물관 서브페이지3 프로젝트 업로드

---
+++ sub_page3/js/function.js
@@ -0,0 +1,27 @@
+$(function(){
+    //header>nav UI
+  const $gnb = $('header > nav > .gnb>li');
+  const $search = $('header > .snb > .snb_title>a')
+  
+  $gnb.on('mouseover', function(){
+    $(this).children('.lnb').stop().fadeIn(200);
+  });
+
+  $gnb.on('mouseout', function(){
+    $(this).children('.lnb').stop().fadeOut(200);
+  });
+
+  $search.on('click', function(evt){
+    evt.preventDefault();
+    $(this).parent().toggleClass('on')
+  });  //end of header>nav UI
+
+  //section#content >mnu클릭이벤트
+  const $mnu= $('section#content>.container>.mnu>li>a');
+
+  $mnu.on('click', function(){
+     $(this).parent().addClass('on').siblings().removeClass('on');
+  });
+  // end of section#content >mnu
+  
+})
